0938f83d5436a8fd0c082bbafab4ceb80f89d688
Matthew <herrigesmt@gmail.com>
2020-07-13 15:48:36 -0700

    finish weather functionality

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ app.use(cors());
 // define PORT as the port value in our .env. if anything is wrong with our .env or dotenv, assign port 3001
 const PORT = process.env.PORT || 3001;
 
-app.get(`/location`, (request, response) => {
+app.get('/location', (request, response) => {
   let city = request.query.city;
-  let cityData = require(`./data/location.json`);
+  let cityData = require('./data/location.json');
   const object = new Location(city, cityData[0]);
   response.send(object);
 })
 
+app.get('/weather', (request, response) => {
+  let weather = require('./data/weather.json');
+  const weatherArray = [];
+  weather.data.forEach(value => {
+    weatherArray.push(new Weather(value));
+  })
+  response.send(weatherArray);
+})
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
@@ -25,4 +33,9 @@ function Location(query, obj){
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
+}
+
+function Weather(obj){
+  this.forecast = obj.weather.description;
+  this.time = obj.valid_datel;
 }
