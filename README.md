38a5204382e8e10a6adb857ed36ba42a685c9448
Matthew <herrigesmt@gmail.com>
2020-07-13 16:43:29 -0700

    finish errors

--- README.md
+++ README.md
@@ -6,9 +6,29 @@ Estimate of time needed to complete: 60 minutes
 
 Start time: 2:44 PM
 
-Finish time: 
+Finish time: 3:15 PM
 
-Actual time needed:
+Actual time needed: 31 Minutes
+
+Number and name of feature: 3. Weather
+
+Estimate of time needed to complete: 45 minutes
+
+Start time: 3:20 PM
+
+Finish time: 3:50 PM
+
+Actual time needed: 30 Minutes
+
+Number and name of feature: 4. Errors
+
+Estimate of time needed to complete: 60 minutes
+
+Start time: 4:00
+
+Finish time: 4:32
+
+Actual time needed: 32 minutes
 
 **AUTHOR**: Matt Herriges
 **VERSION**: 1.0.0
--- server.js
+++ server.js
@@ -9,33 +9,48 @@ app.use(cors());
 // define PORT as the port value in our .env. if anything is wrong with our .env or dotenv, assign port 3001
 const PORT = process.env.PORT || 3001;
 
+const errObj = {
+  status: 500,
+  responseText: 'Sorry, something went wrong',
+}
+
 app.get('/location', (request, response) => {
   let city = request.query.city;
   let cityData = require('./data/location.json');
-  const object = new Location(city, cityData[0]);
-  response.send(object);
+  try {
+
+    const object = new Location(city, cityData[0]);
+    response.send(object);
+  } catch (error) {
+    console.log('ERROR', error);
+    response.status(errObj.status).send(errObj.responseText);
+  }
 })
 
 app.get('/weather', (request, response) => {
   let weather = require('./data/weather.json');
+  console.log(request.query.search_query);
+
   const weatherArray = [];
+
   weather.data.forEach(value => {
     weatherArray.push(new Weather(value));
   })
   response.send(weatherArray);
+
 })
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
 
-function Location(query, obj){
+function Location(query, obj) {
   this.search_query = query;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 }
 
-function Weather(obj){
+function Weather(obj) {
   this.forecast = obj.weather.description;
   this.time = obj.valid_date;
 }
